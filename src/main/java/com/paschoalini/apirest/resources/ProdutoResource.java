e384ad97fe0580fca25bcb62121c0697f31d6f8d
Glauber Roberto Paschoalini <prglauber@gmail.com>
2020-01-15 12:38:19 -0300

    Endpoint /produto para cadastrar novo produto

--- src/main/java/com/paschoalini/apirest/resources/ProdutoResource.java
+++ src/main/java/com/paschoalini/apirest/resources/ProdutoResource.java
@@ -3,10 +3,7 @@ package com.paschoalini.apirest.resources;
 import com.paschoalini.apirest.models.Produto;
 import com.paschoalini.apirest.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
 import java.util.List;
@@ -29,5 +26,10 @@ public class ProdutoResource {
     @GetMapping("/produto/{id}")
     public Produto listaProdutoUnico(@PathVariable(value = "id") Long id) {
         return produtoRepository.findById(id).get();
+    }
+
+    @PostMapping("/produto")
+    public Produto salvaProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
     }
 }
