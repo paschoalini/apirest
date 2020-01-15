21249582be3a0390c550a1fbaffb8b685929112a
Glauber Roberto Paschoalini <prglauber@gmail.com>
2020-01-15 11:50:29 -0300

    Projeto criado

---
+++ src/test/java/com/paschoalini/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paschoalini.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
