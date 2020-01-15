65f3a42e567f20d91bb94cbdb57d298e69614907
Glauber Roberto Paschoalini <prglauber@gmail.com>
2020-01-15 12:33:09 -0300

    Endpoint /produto/{id} para listar um produto

--- src/main/java/com/paschoalini/apirest/resources/ProdutoResource.java
+++ src/main/java/com/paschoalini/apirest/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ import com.paschoalini.apirest.models.Produto;
 import com.paschoalini.apirest.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class ProdutoResource {
     @GetMapping("/produtos")
     public List<Produto> listaProdutos() {
         return produtoRepository.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    public Produto listaProdutoUnico(@PathVariable(value = "id") Long id) {
+        return produtoRepository.findById(id).get();
     }
 }
